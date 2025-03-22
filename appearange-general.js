3c3d73e2481215701de6a04438d1ec84f1b54daa
hadiserhan <hadi@softview-me.com>
2025-03-22 22:40:21 +0200

    first

---
+++ appearange-general.js
@@ -0,0 +1,64 @@
+function OpenConnectionForm()
+{
+  if(isConnected)
+    showConfirmDialog();
+  else
+    connectSerial();  
+}
+
+function ConnectedSerialPort()
+{
+  isConnected = true;
+  connectionType = "Serial";
+  // $("#btConnect").removeClass("notConnectedButton");
+  // $("#btConnect").addClass("connectedButton");
+}
+
+function DisconnectedSerialPort()
+{
+  isConnected = false;
+  // $("#btConnect").removeClass("connectedButton");
+  // $("#btConnect").addClass("notConnectedButton");
+}
+
+function showConfirmDialog()
+{
+    // swal({
+    //       //title: "Are you sure?",
+    //       text: ModalConfirmDialogText,
+    //       icon: "info",
+    //       buttons: [CancelText, OkeyText],
+    //       dangerMode: false,
+    // }).then((willClose) => {
+    //     if (willClose) {
+    //         disConnect()
+    //     }
+    // });
+    disConnect()
+}
+
+function disConnect()
+{
+    if(connectionType == "Serial")
+    {
+      disConnectSerial();
+    }
+    else if(connectionType == "WebRepl")
+    {
+      disConnectWebRepl();
+    }
+    else if(connectionType == "BLE")
+    {
+      disConnectBLE();
+    }
+}
+
+
+// bluetooth
+function OpenBLEForm()
+{
+  if(isConnected)
+    showConfirmDialog();
+  else
+    connectToBle();  
+}
